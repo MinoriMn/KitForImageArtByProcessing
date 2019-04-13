39cca087fd64eb407b977a762aa66856320cd762
MinoriMn <lucky_bluebrowneye10280915@au.com>
2019-04-13 11:29:14 +0900

    [WIP]Queue 導入前

--- src/processingKit/KeyFrameController.kt
+++ src/processingKit/KeyFrameController.kt
@@ -4,46 +4,71 @@ package processingKit
  * KeyFrameController
  * @param initInfos Array<キー名:String, Triple<初期座標(絶対座標のみ):Float, 座標指定タイプ:TransitionType , スタートフレーム: Int>>
  */
-class KeyFrameController(initInfos: Array<String, Array<Pair <> >>){
+class KeyFrameController(initInfos: Array<String>){
     private val keyNamesMap: Map<String, Int> //キー名 -> 連番
-    private val tripleOfFrame_Position_TransitionType: ArrayList<ArrayList<Triple<Int, Float, TransitionType>>> //連番 -> (遷移基準フレーム, 遷移座標)
-    private val currentKeyFrames: ArrayList<Int> //現在実行中のキーフレーム番目
+    private val listOfKeyFrameList: ArrayList<ArrayList<KeyFrameData>> //連番 -> キー番 -> (遷移基準フレーム, 遷移座標, 遷移タイプ)
+    private val currentKeyFrameNumbers: ArrayList<Int> //現在実行中のキーフレーム番目
     private val prePositions: ArrayList<Float> //前フレーム位置
+    private val nextFrames: ArrayList<Int> //次フレーム
+
+    private var frame = -1
+        get() = frame
 
     val NOT_ON_KEY_FRAME = Float.NaN
 
     init {
         keyNamesMap = mutableMapOf()
-        tripleOfFrame_Position_TransitionType = ArrayList()
-        currentKeyFrames = ArrayList()
+        listOfKeyFrameList = ArrayList()
+        currentKeyFrameNumbers = ArrayList()
         prePositions = ArrayList()
+        nextFrames = ArrayList()
 
         for(i in 0 until initInfos.size){
-            val info = initInfos[i]
             keyNamesMap.plus(Pair(initInfos[i], i))
 
-            val arrayOfFirstFrameAndPosition = ArrayList<Pair<Int, Float>>()
-            tripleOfFrame_Position_TransitionType.add(arrayOfFirstFrameAndPosition)
-            currentKeyFrames.add(-1)
+            val arrayOfFirstFrameAndPosition = ArrayList<KeyFrameData>()
+            listOfKeyFrameList.add(arrayOfFirstFrameAndPosition)
+            currentKeyFrameNumbers.add(-1)
             prePositions.add(NOT_ON_KEY_FRAME)
+            nextFrames.add(0)
         }
 
     }
 
-    //* ()
-    private fun initPairOfFrameAndPosition(){
+    fun update(){
+        frame++
+        for (index in 0 until keyNamesMap.size){
+            if(nextFrames[index] == frame){
 
+            }
+        }
     }
 
-    data class AddKeyFrameInfo()
+    fun addPairOfFrameAndPosition(keyName: String, addKeyFrames: ArrayList<AddKeyFrameData>){
+        val position = keyNamesMap[keyName]
+        if(position != null){
+            val keyFrameList = listOfKeyFrameList[position]
+            var lastKeyFrame = if(keyFrameList.isNotEmpty()) keyFrameList.last() else KeyFrameData(-1, NOT_ON_KEY_FRAME, TransitionType.NOT_ON_FRAME)
+            addKeyFrames.forEach {
+                val frame = (it.frame * TheNumberOfFramesMultiplyUtil.ratio).toInt()
+                val position = if(it.positionType == PositionType.Absolute) it.position else it.position + lastKeyFrame.position
+                val keyFrameData = KeyFrameData(frame, position, it.transitionType)
+                keyFrameList.add(keyFrameData)
+                lastKeyFrame = keyFrameData
+            }
+        }
+    }
 
+    private data class KeyFrameData(val frame: Int, val position: Float, val transitionType: TransitionType )
+    data class AddKeyFrameData(val frame: Int, val position: Float, val transitionType: TransitionType = TransitionType.Linear, val positionType: PositionType = PositionType.Absolute)
 
     enum class TransitionType{
         Freeze,
         Linear,
         EaseIn,
-        EaseOut,
-        NOT_ON_KEY_FRAME,
+        EaseOut ,
+        EaseInOut,
+        NOT_ON_FRAME,
         END_OF_KEY_FRAME
     }
 
@@ -51,6 +76,4 @@ class KeyFrameController(initInfos: Array<String, Array<Pair <> >>){
         Absolute,
         Relative
     }
-
-
 }
